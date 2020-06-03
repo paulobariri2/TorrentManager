9ff0fb28eadb37a21379ed9b587bf94520bf23c0
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-06-03 05:34:11 -0300

    Creating spring boot project

---
+++ torrentmanager/src/main/java/torrent/manager/app/TorrentManager.java
@@ -0,0 +1,13 @@
+package torrent.manager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorrentManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorrentManager.class, args);
+	}
+
+}
