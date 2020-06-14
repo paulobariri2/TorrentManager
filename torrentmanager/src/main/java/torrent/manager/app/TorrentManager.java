38a3cf5ef9587c3f75c47949ad6ab933f163d843
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-06-14 16:43:18 -0300

    Removing unused imports

--- torrentmanager/src/main/java/torrent/manager/app/TorrentManager.java
+++ torrentmanager/src/main/java/torrent/manager/app/TorrentManager.java
@@ -1,13 +1,10 @@
 package torrent.manager.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import torrent.manager.deamon.DownloadDeamon;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"torrent.manager.model"})
