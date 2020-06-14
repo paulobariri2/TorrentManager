d1646a26c83305c000af4554e504f329c1673fb2
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-06-14 16:37:42 -0300

    Removing unused import

--- torrentmanager/src/main/java/torrent/manager/controller/TorrentManagementController.java
+++ torrentmanager/src/main/java/torrent/manager/controller/TorrentManagementController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import torrent.manager.dao.TorrentRepository;
 import torrent.manager.model.Torrent;
