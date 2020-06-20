d0a1e89d863580de9bfeb02b8092b53a9b9a06b8
Paulo Barbieri <paulo@MacBook-Air-de-Paulo.local>
2020-06-19 23:36:20 -0300

    Adding REST to get and update torrent

--- torrentmanager/src/main/java/torrent/manager/controller/TorrentManagementController.java
+++ torrentmanager/src/main/java/torrent/manager/controller/TorrentManagementController.java
@@ -1,11 +1,14 @@
 package torrent.manager.controller;
 
 import java.sql.Timestamp;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +27,6 @@ public class TorrentManagementController {
     @PostMapping(path = "/download")
     public ResponseEntity<Torrent> download(@RequestBody Torrent torrent) {
         try {
-            torrent.setPid("111");
             Timestamp nowDateTime = new Timestamp(System.currentTimeMillis());
             torrent.setDateTime(nowDateTime);
             torrent.setStatus(TorrentStatusTP.READY);
@@ -35,5 +37,28 @@ public class TorrentManagementController {
         }
 
         return new ResponseEntity<Torrent>(torrent, HttpStatus.CREATED);
+    }
+
+    @PostMapping(path = "/update")
+    public ResponseEntity<Torrent> updateTorrent(@RequestBody Torrent torrent) {
+        long id = torrent.getId();
+        Optional<Torrent> optional = torrentRepository.findById(id);
+        if (optional.isPresent()) {
+            torrentRepository.save(torrent);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Torrent>(torrent, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Torrent> getTorrent(@PathVariable long id) {
+        Optional<Torrent> optional = torrentRepository.findById(id);
+        if (!optional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Torrent torrent = optional.get();
+        return new ResponseEntity<Torrent>(torrent, HttpStatus.OK);
     }
 }
